d226c1276b795bdf9231fee444b2915b9d354c7c
Ryan Toth <ryan.toth@hotmail.com>
2021-06-26 14:31:40 -1000

    basic Leadership page

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,56 +1,53 @@
 import React from 'react';
-import { Layout } from 'antd';
+import { Layout, Row, Col, Image } from 'antd';
+import ReactMarkdown from 'react-markdown'
+import ReactPlayer from "react-player"
+import './AboutUs.css'
+import PCTLogo from './pics/AboutUs.jpg'
 const { Content } = Layout;
 
 const AboutUs = () => {
-  // Figure out how to link to specific part of page
-    return (
-      <Content className="content">
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <span id="pct" className="pct">PCT</span>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <span id="fraternity-video" className="fraternity-video">Video</span>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <span id="pillars" className="pillars">Pillars</span>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-      </Content>
-    );
+  return (
+    <Content className="content">
+      <span id="pct" className="pct"></span>
+      <Row>
+        <Col className="text-about-col" span={11}>
+        <h2 className="heading">About Phi Chi Theta</h2>
+          <ReactMarkdown className="text-about">Phi Chi Theta is a National Coeducational Professional Fraternity in Business and Economics. Phi Chi Theta was founded on June 16, 1924 in Chicago, Illinois. The mission of Phi Chi Theta is to:
+
+            Provide an opportunity to develop and practice those professional leadership skills and abilities necessary to succeed in the business community; Provide a local and national network to share resources, ideas and concepts; Instill in its membership those values, codes and creeds which will enable participation in a rapidly changing world: and Enable members to develop the business astuteness necessary to achieve high esteem and success in their chosen fields.
+
+            Today, the Phi Chi Theta National Chapter is comprised of 41 Collegiate and Alumni Chapters with over 23,000 members across the country. Phi Chi Theta is a member of and affiliated with the Association to Advance Collegiate Schools of Business (AACSB), the Professional Fraternity Association (PFA), and the Professional Fraternity Executive Association (PFEA).</ReactMarkdown>
+        </Col>
+        <Col className="pctlogo-col" span={11}>
+          <Image src={PCTLogo} preview={false}></Image>
+        </Col>
+      </Row>
+      <span id="fraternity-video" className="fraternity-video"></span>
+      <h2 className="heading">PCT At Michigan</h2>
+      <Col className="video-col">
+        <ReactPlayer className="video" url="https://www.youtube.com/watch?v=9eBxa_B8EO8"/>
+      </Col>
+      <span id="pillars" className="pillars">Pillars</span>
+      {/* Opportunity to do something cooooool here with the UI */}
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+    </Content>
+  );
 }
 
 export default AboutUs;
--- src/components/Leadership/Leadership.js
+++ src/components/Leadership/Leadership.js
@@ -1,52 +1,170 @@
 import React from 'react';
-import { Layout } from 'antd';
+import { Layout, Col, Row, Image } from 'antd';
+import elPrez from './pics/Krisha.jpg'
+import vpi from './pics/Jason.jpg'
+import vpx from './pics/Theo.jpg'
+import vpf from './pics/Ian.jpg'
+import vpOp1 from './pics/Jack.jpg'
+import vpOp2 from './pics/Josie.jpg'
+import marketing1 from './pics/Michaela.jpg'
+// import marketing2 from './pics/Chin.jpg'
+// import alumni from './pics/Linh.jpg'
+// import community1 from './pics/Anjali.jpg'
+// import community2 from './pics/Sahaja.jpg'
+// import fundraising1 from './pics/Carly.jpg'
+// import fundraising2 from './pics/Katie.jpg'
+// import IR1 from './pics/Yaz.jpg'
+// import IR2 from './pics/Ava.jpg'
+// import Member1 from './pics/Jajoo.jpg'
+// import Member2 from './pics/Erik.jpg'
+// import Social1 from './pics/Spiro.jpg'
+import Social2 from './pics/Adi.jpg'
+import Parent from './pics/Jharna.jpg'
+// import Prof1 from './pics/Spencer.jpg'
+import Prof2 from './pics/Anne.jpg'
+import Academic1 from './pics/Alexa.jpg'
+// import Academic2 from './pics/Mia.jpg'
+import DEI1 from './pics/Jeevin.jpg'
+// import DEI2 from './pics/Audrey.jpg'
+import tech1 from './pics/Ryan.jpg'
+// import tech2 from './pics/Ant.jpg'
+// import wellness1 from './pics/Aakash.jpg'
+import wellness2 from './pics/Sarah.jpg'
+
+
 const { Content } = Layout;
 
 const Leadership = () => {
     return (
       <Content className="content">
-           <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <span id="executive-board" className="executive-board">Executive Board</span>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <span id="directors" className="directors">Directors</span>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
+          <span id="executive-board" className="executive-board"></span>
+          <h1 className="heading">Executive Board</h1>
+          <Row>
+            <Col span={8}>
+              <Image src={elPrez} preview={false}></Image>
+              <h2 className="board-name">Krisha Monpara</h2>
+              <h4 className="board-position">President</h4>
+              <p className="board-bio">Krisha is a junior studying Economics. From Pittsburgh, Pennsylvania, she enjoys running, playing the piano, and spending time with her friends and family. Her internship experience includes Wealth Management at Cetera Financial Investors, the Women’s Advisory Program in Global Investment Banking at RBC, and as an incoming Financial Analyst at Amazon in Seattle.</p>
+            </Col>
+            <Col span={8}>
+              <Image src={vpi} preview={false}></Image>
+              <h2 className="board">Jason Stahlman</h2>
+              <h4 className="board-position">VP Internal</h4>
+              <p className="board-bio"></p>
+            </Col>
+            <Col span={8}>
+              <Image src={vpx} preview={false}></Image>
+              <h2 className="board">Theo Kardasis</h2>
+              <h4 className="board-position">VP External</h4>
+              <p className="board-bio"></p>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={8}>
+              <Image src={vpf} preview={false}></Image>
+              <h2 className="board">Ian Kim</h2>
+              <h4 className="board-position">VP Finance</h4>
+              <p className="board-bio"></p>
+            </Col>
+            <Col span={8}>
+              <Image src={vpOp1} preview={false}></Image>
+              <h2 className="board">Jack Xiao</h2>
+              <h4 className="board-position">VP Operations</h4>
+              <p className="board-bio"></p>
+            </Col>
+            <Col span={8}>
+              <Image src={vpOp2} preview={false}></Image>
+              <h2 className="board">Josie Bachus</h2>
+              <h4 className="board-position">VP Operations</h4>
+              <p className="board-bio"></p>
+            </Col>
+          </Row>
+          <span id="directors" className="directors"></span>
+          <h1 className="heading">Directors</h1>
+          <Row>
+            <Col span={6}>
+              <Image src={Parent} preview={false}></Image>
+            </Col>
+            <Col span={6}>
+              {/* <Image src={alumni} preview={false}></Image> */}
+            </Col>
+            <Col span={6}>
+              {/* <Image src={Prof1} preview={false}></Image> */}
+            </Col>
+            <Col span={6}>
+              <Image src={Prof2} preview={false}></Image>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={6}>
+              {/* <Image src={Member1} preview={false}></Image> */}
+            </Col>
+            <Col span={6}>
+              {/* <Image src={Member2} preview={false}></Image> */}
+            </Col>
+            <Col span={6}>
+              <Image src={Academic1} preview={false}></Image>
+            </Col>
+            <Col span={6}>
+              {/* <Image src={Academic2} preview={false}></Image> */}
+            </Col>
+          </Row>
+          <Row>
+            <Col span={6}>
+              {/* <Image src={wellness1} preview={false}></Image> */}
+            </Col>
+            <Col span={6}>
+              <Image src={wellness2} preview={false}></Image>
+            </Col>
+            <Col span={6}>
+              <Image src={DEI1} preview={false}></Image>
+            </Col>
+            <Col span={6}>
+              {/* <Image src={DEI2} preview={false}></Image> */}
+            </Col>
+          </Row>
+          <Row>
+            <Col span={6}>
+              {/* <Image src={IR1} preview={false}></Image> */}
+            </Col>
+            <Col span={6}>
+              {/* <Image src={IR2} preview={false}></Image> */}
+            </Col>
+            <Col span={6}>
+              {/* <Image src={Social1} preview={false}></Image> */}
+            </Col>
+            <Col span={6}>
+              <Image src={Social2} preview={false}></Image>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={6}>
+              {/* <Image src={fundraising1} preview={false}></Image> */}
+            </Col>
+            <Col span={6}>
+              {/* <Image src={fundraising2} preview={false}></Image> */}
+            </Col>
+            <Col span={6}>
+              <Image src={marketing1} preview={false}></Image>
+            </Col>
+            <Col span={6}>
+              {/* <Image src={marketing2} preview={false}></Image> */}
+            </Col>
+          </Row>
+          <Row>
+            <Col span={6}>
+              {/* <Image src={community1} preview={false}></Image> */}
+            </Col>
+            <Col span={6}>
+              {/* <Image src={community2} preview={false}></Image> */}
+            </Col>
+            <Col span={6}>
+              <Image src={tech1} preview={false}></Image>
+            </Col>
+            <Col span={6}>
+              {/* <Image src={tech2} preview={false}></Image> */}
+            </Col>
+          </Row>
       </Content>
     );
 }
