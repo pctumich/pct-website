8f0a1c8e63b36be1e296dd1d6a3bba40417e35e6
aqzhu <aqzhu@umich.edu>
2021-07-18 07:06:58 -0400

    Remove slideshow margin, remove arrows and shorten fade duration

--- src/components/Home/slideshow.js
+++ src/components/Home/slideshow.js
@@ -9,33 +9,31 @@ import slide_3 from './pics/IanKim.jpg'
 
 const Slideshow = () => {
     return (
-        <div>
-            <div className="slide-container">
-                <div className="text-container">
-                    <h1 className="text-title">
-                        PHI CHI THETA 
-                        <hr className="text-hr"></hr>
-                    </h1>
-                    <h3 className="text-description">Michigan's Leading Professional Business Fraternity</h3>
-                </div>
-                <Fade pauseOnHover={false} duration={7000}>
-                    <div className="each-fade">
-                        <div>
-                            <img src={slide_1} />
-                        </div>
-                    </div>
-                    <div className="each-fade">
-                        <div>
-                            <img src={slide_2} />
-                        </div>
-                    </div>
-                    <div className="each-fade">
-                        <div>
-                            <img src={slide_3} />
-                        </div>
-                    </div>
-                </Fade>
+        <div className="slide-container">
+            <div className="text-container">
+                <h1 className="text-title">
+                    PHI CHI THETA 
+                    <hr className="text-hr"></hr>
+                </h1>
+                <h3 className="text-description">Michigan's Leading Professional Business Fraternity</h3>
             </div>
+            <Fade pauseOnHover={false} duration={5000} arrows={false}>
+                <div className="each-fade">
+                    <div>
+                        <img src={slide_1} />
+                    </div>
+                </div>
+                <div className="each-fade">
+                    <div>
+                        <img src={slide_2} />
+                    </div>
+                </div>
+                <div className="each-fade">
+                    <div>
+                        <img src={slide_3} />
+                    </div>
+                </div>
+            </Fade>
         </div>
     )
 };
